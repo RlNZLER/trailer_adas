d33c01643c16cb74868c9b20f528d5c93f8902cc
RlNZLER <amelpvarghese@gmail.com>
2024-07-30 02:39:52 +0100

    Visulaize the position and orientation of robot in rviz. The orientation seem to be inverse. Need to check this and try to fix it. Or go back to simple steering controller.

--- src/trailer_controller/trailer_controller/ackermann_controller.py
+++ src/trailer_controller/trailer_controller/ackermann_controller.py
@@ -3,13 +3,14 @@
 import rclpy
 from rclpy.node import Node
 from sensor_msgs.msg import Joy
-from geometry_msgs.msg import TwistStamped
+from geometry_msgs.msg import TwistStamped, TransformStamped
 from sensor_msgs.msg import JointState
 from rclpy.time import Time
 from rclpy.constants import S_TO_NS
 from nav_msgs.msg import Odometry
 from math import cos, sin
 from tf_transformations import quaternion_from_euler
+from tf2_ros import TransformBroadcaster
 
 
 class SimpleController(Node):
@@ -54,6 +55,12 @@ class SimpleController(Node):
         self.odom_msg_.pose.pose.orientation.y = 0.0
         self.odom_msg_.pose.pose.orientation.z = 0.0
         self.odom_msg_.pose.pose.orientation.w = 1.0
+        
+        self.br_ = TransformBroadcaster(self)
+        self.transform_stamped_ = TransformStamped()
+        self.transform_stamped_.header.frame_id = "odom"
+        self.transform_stamped_.child_frame_id = "base_footprint"
+        
         
         # Initialize speed variables
         self.current_speed = 0.0
@@ -145,7 +152,16 @@ class SimpleController(Node):
         self.odom_msg_.twist.twist.linear.x = linear
         self.odom_msg_.twist.twist.angular.z = angular
         
+        self.transform_stamped_.transform.translation.x = self.x_
+        self.transform_stamped_.transform.translation.y = self.y_
+        self.transform_stamped_.transform.rotation.x = q[0]
+        self.transform_stamped_.transform.rotation.y = q[1]
+        self.transform_stamped_.transform.rotation.z = q[2]
+        self.transform_stamped_.transform.rotation.w = q[3]
+        self.transform_stamped_.header.stamp = self.get_clock().now().to_msg()
+        
         self.odom_pub_.publish(self.odom_msg_)
+        self.br_.sendTransform(self.transform_stamped_)
         
         # self.get_logger().info(f"Linear velocity: {linear}, Angular velocity: {angular}")
         # self.get_logger().info(f"Position: ({self.x_}, {self.y_}), Orientation: {self.theta_}")
