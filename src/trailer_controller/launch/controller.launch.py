858cc35813b822be4c9fd7669995ec21bdf327e5
RlNZLER <amelpvarghese@gmail.com>
2024-08-05 23:52:34 +0100

    Added joystick to the control launch file

--- src/trailer_controller/launch/controller.launch.py
+++ src/trailer_controller/launch/controller.launch.py
@@ -1,7 +1,9 @@
+import os
 from launch import LaunchDescription
 from launch_ros.actions import Node
 from launch.actions import DeclareLaunchArgument, OpaqueFunction
 from launch.substitutions import LaunchConfiguration
+from ament_index_python.packages import get_package_share_directory
 
 
 def noisy_controller(context, *args, **kwargs):
@@ -120,6 +122,19 @@ def generate_launch_description():
         function = noisy_controller
     )
     
+    joy_node = Node(
+        package='joy',
+        executable='joy_node',
+        name='joystick',
+        parameters = [os.path.join(get_package_share_directory('trailer_controller'), 'config', 'joy_config.yaml')]
+    )
+    
+    joy_teleop = Node(
+        package='joy_teleop',
+        executable='joy_teleop',
+        parameters = [os.path.join(get_package_share_directory('trailer_controller'), 'config', 'joy_teleop.yaml')]
+    )
+    
     return LaunchDescription([
         wheel_radius_arg,
         wheel_separation_arg,
@@ -131,5 +146,7 @@ def generate_launch_description():
         simple_velocity_controller_spawner,
         steering_position_controller_spawner,
         ackermann_controller_py,
-        noisy_controller_launch
+        noisy_controller_launch,
+        joy_node,
+        joy_teleop
     ])
